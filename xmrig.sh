27682fcf13a45ec31e2bce4cbd4bda8f449f8482
Eric Meeks <efmeeks@pm.me>
2021-08-16 22:28:39 -0500

    Create xmrig.sh

---
+++ xmrig.sh
@@ -0,0 +1,19 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-install -y screen
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-focal-x64.tar.gz
+tar -xvzf xmrig-6.14.0-focal-x64.tar.gz
+mv xmrig-6.14 xmrig
+
+wget https://raw.githubusercontent.com/efmeeks/xmrig/main/myriade.txt
+wget https://raw.githubusercontent.com/efmeeks/xmrig/main/xmrpool.eu.txt
+mv myriade.txt xmrpool.eu.txt xmrig
+
+echo "screen ~/xmrig/xmrig" > start-mining
+chmod +x start-mining
+
+echo "kill \$(ps aux | grep xmrig | grep -v grep | awk '{print $2}')" > stop-mining
+chmod +x stop-mining
+
+echo Done
